bf18d9a129a0594302762d31e052ae2faba27853
Siddharth Vyas <siddharth.vyas9299@gmail.com>
2021-04-20 09:10:36 +0530

    Second Major1

---
+++ frontend/src/store.js
@@ -0,0 +1,48 @@
+import {createStore,combineReducers,applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import {productListReducers,productDetailsReducers,productTopRatedReducers} from './reducers/productReducers'
+import {cartReducer} from './reducers/cartReducers'
+import {userLoginReducers,userRegisterReducers,userDetailsReducers,userUpdateProfileReducers} from './reducers/userReducers'
+import {orderCreateReducer,orderDetailsReducer,orderPayReducer,orderListMyReducer} from './reducers/orderReducers'
+
+const reducer= combineReducers({
+    productList:productListReducers,
+    productDetails:productDetailsReducers,
+    cart:cartReducer,
+    userLogin:userLoginReducers,
+    userRegister:userRegisterReducers,
+    userDetails:userDetailsReducers,
+    userUpdateProfile:userUpdateProfileReducers,
+    orderCreate:orderCreateReducer,
+    orderDetails:orderDetailsReducer,
+    orderPay:orderPayReducer,
+    orderListMy:orderListMyReducer,
+    productTopRated:productTopRatedReducers
+})
+
+const cartItemsFromStorage=localStorage.getItem('cartItems') ? 
+        JSON.parse(localStorage.getItem('cartItems')): []
+
+const userInfoFromStorage=localStorage.getItem('userInfo') ? 
+        JSON.parse(localStorage.getItem('userInfo')): null
+
+const shippingAddressFromStorage=localStorage.getItem('shippingAddress') ? 
+        JSON.parse(localStorage.getItem('shippingAddress')): {}
+
+
+
+const initialState={
+    cart:{
+        cartItems:cartItemsFromStorage,
+        shippingAddress:shippingAddressFromStorage
+    },
+    userLogin: {userInfo: userInfoFromStorage},
+}
+
+const middleware = [thunk]
+
+const store =createStore(reducer,initialState,
+    composeWithDevTools(applyMiddleware(...middleware))) 
+
+export default store
